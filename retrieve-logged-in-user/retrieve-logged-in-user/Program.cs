d606e1132c253018160d0b863d40e50a7d5e4db7
Dakeezin <133807800+Dakeezin@users.noreply.github.com>
2023-05-17 09:06:35 -0300

    Add files via upload

---
+++ retrieve-logged-in-user/retrieve-logged-in-user/Program.cs
@@ -0,0 +1,18 @@
+﻿using Projeto.Data.Modelo;
+using Projeto.Data.Repositorio;
+
+namespace Projeto.Aplicacao.Models
+{
+    public class LoginModel
+    {
+        /// <summary>
+        /// Método para recuperar o usuário logado no sistema
+        /// </summary>
+        /// <returns></returns>
+        public BVC_Projeto GetUsuarioLogado(string strLogin)
+        {
+            Repositorio<BVC_Projeto> objUsuarioRep = new Repositorio<BVC_Projeto>();
+            return objUsuarioRep.Find(x => x.Login == strLogin);
+        }
+    }
+}
